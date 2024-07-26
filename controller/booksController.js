b65f56ee2a785cf01b72833798ddd66d92808ac9
Robert <robertyc.chang@wpgholdings.com>
2024-07-27 00:16:06 +0800

    remove redis

--- controller/booksController.js
+++ controller/booksController.js
@@ -8,120 +8,122 @@ const DEFAULT_EXPIRATION = 3600;
 // @route GET /books
 // @access Private
 const getAllBooks = asyncHandler(async (req, res) => {
-    const books = await getOrSetCache("books", async () => {
-        const data = await Book.find().lean();
-        return data;
-    });
+  //   const books = await getOrSetCache("books", async () => {
+  //     const data = await Book.find().lean();
+  //     return data;
+  //   });
 
-    if (!books?.length) {
-        res.status(400).json({ message: "Not books found" });
-    }
-    res.json(books);
+  const books = await Book.find().lean();
+
+  if (!books?.length) {
+    res.status(400).json({ message: "Not books found" });
+  }
+  res.json(books);
 });
 
 const getOrSetCache = async (key, callback) => {
-    const response = await client.get(key);
-    if (response) return JSON.parse(response);
-    const freshData = await callback();
-    client.setEx(key, DEFAULT_EXPIRATION, JSON.stringify(freshData));
+  const response = await client.get(key);
+  if (response) return JSON.parse(response);
+  const freshData = await callback();
+  client.setEx(key, DEFAULT_EXPIRATION, JSON.stringify(freshData));
 
-    return freshData;
+  return freshData;
 };
 
 // @desc Create new book
 // @route POST /books
 // @access Private
 const createNewBook = asyncHandler(async (req, res) => {
-    const { bookname, author, publishYear, imgSrc, price, summary, type } =
-        req.body;
+  const { bookname, author, publishYear, imgSrc, price, summary, type } =
+    req.body;
 
-    if (
-        !bookname ||
-        !price ||
-        !author ||
-        !publishYear ||
-        !summary ||
-        !Array.isArray(type) ||
-        !type.length
-    ) {
-        return res.status(400).json({ message: "All fields are required" });
-    }
+  if (
+    !bookname ||
+    !price ||
+    !author ||
+    !publishYear ||
+    !summary ||
+    !Array.isArray(type) ||
+    !type.length
+  ) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
 
-    const bookObject = {
-        bookname,
-        price,
-        author,
-        publishYear,
-        summary,
-        type,
-        imgSrc,
-    };
+  const bookObject = {
+    bookname,
+    price,
+    author,
+    publishYear,
+    summary,
+    type,
+    imgSrc,
+  };
 
-    const book = await Book.create(bookObject);
-    if (book) {
-        res.status(201).json({ message: `New book ${bookname} created` });
-    } else {
-        res.status(400).json({ message: "Invalid book data received" });
-    }
+  const book = await Book.create(bookObject);
+  if (book) {
+    res.status(201).json({ message: `New book ${bookname} created` });
+  } else {
+    res.status(400).json({ message: "Invalid book data received" });
+  }
 });
 
 // @desc update a book
 // @route PATCH /books
 // @access Private
 const updateBook = asyncHandler(async (req, res) => {
-    const { bookname, price, author, publishYear, summary, type, id, imgSrc } =
-        req.body;
+  const { bookname, price, author, publishYear, summary, type, id, imgSrc } =
+    req.body;
 
-    if (
-        !bookname ||
-        !price ||
-        !author ||
-        !publishYear ||
-        !summary ||
-        !imgSrc ||
-        !Array.isArray(type) ||
-        !type.length
-    ) {
-        return res.status(400).json({ message: "All fields are required" });
-    }
+  if (
+    !bookname ||
+    !price ||
+    !author ||
+    !publishYear ||
+    !summary ||
+    !imgSrc ||
+    !Array.isArray(type) ||
+    !type.length
+  ) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
 
-    const book = await Book.findById(id).exec();
-    if (!book) {
-        return res.status(400).json({ message: "Book not found" });
-    }
+  const book = await Book.findById(id).exec();
+  if (!book) {
+    return res.status(400).json({ message: "Book not found" });
+  }
 
-    book.bookname = bookname;
-    book.price = price;
-    book.author = author;
-    book.publishYear = publishYear;
-    book.summary = summary;
-    book.type = type;
-    book.imgSrc = imgSrc;
+  book.bookname = bookname;
+  book.price = price;
+  book.author = author;
+  book.publishYear = publishYear;
+  book.summary = summary;
+  book.type = type;
+  book.imgSrc = imgSrc;
 
-    const updatedBook = await book.save();
+  const updatedBook = await book.save();
 
-    res.json({ message: `${updatedBook.bookname} update` });
+  res.json({ message: `${updatedBook.bookname} update` });
 });
 
 // @desc Delete a book
 // @route DELETE /books
 // @access Private
 const deleteBook = asyncHandler(async (req, res) => {
-    const { id } = req.body;
-    if (!id) {
-        return res.status(400).json({ message: "Book ID Required" });
-    }
+  const { id } = req.body;
+  if (!id) {
+    return res.status(400).json({ message: "Book ID Required" });
+  }
 
-    const book = await Book.findById(id).exec();
-    if (!Book) {
-        return res.status(400).json({ message: "Book not found" });
-    }
+  const book = await Book.findById(id).exec();
+  if (!Book) {
+    return res.status(400).json({ message: "Book not found" });
+  }
 
-    await book.deleteOne();
+  await book.deleteOne();
 
-    const reply = `Bookname ${book.bookname} with ID ${book._id} deleted`;
+  const reply = `Bookname ${book.bookname} with ID ${book._id} deleted`;
 
-    res.json(reply);
+  res.json(reply);
 });
 
 module.exports = { getAllBooks, createNewBook, updateBook, deleteBook };
--- server.js
+++ server.js
@@ -11,27 +11,27 @@ console.log(process.env.NODE_ENV);
 
 connectDB();
 
-if (!client.isOpen) {
-    client.connect();
-}
+// if (!client.isOpen) {
+//   client.connect();
+// }
 
-client.on("error", (err) => console.log("Redis Client Error", err));
+// client.on("error", (err) => console.log("Redis Client Error", err));
 
-client.on("ready", () => {
-    console.log("Connected on Redis");
-});
+// client.on("ready", () => {
+//     console.log("Connected on Redis");
+// });
 
 mongoose.connection.once("open", () => {
-    console.log("Connected on MongoDB");
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
+  console.log("Connected on MongoDB");
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 });
 
 mongoose.connection.on("error", (err) => {
-    console.log(err);
-    logEvent(
-        `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
-        "mongoErrLog.log"
-    );
+  console.log(err);
+  logEvent(
+    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+    "mongoErrLog.log"
+  );
 });
